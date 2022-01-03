98ab1ed9730bb24fbbe3461703a7a936ae63407f
myungsooo <audtn0508@naver.com>
2022-01-03 23:35:56 +0900

    프로젝트 생성
    2022.01.03 월요일 시작

---
+++ MyTestProject/Intermediate/Build/Mac/x86_64/UE4Editor/Inc/MyTestProject/MyTestProjectGameModeBase.gen.cpp
@@ -0,0 +1,87 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "MyTestProject/MyTestProjectGameModeBase.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeMyTestProjectGameModeBase() {}
+// Cross Module References
+	MYTESTPROJECT_API UClass* Z_Construct_UClass_AMyTestProjectGameModeBase_NoRegister();
+	MYTESTPROJECT_API UClass* Z_Construct_UClass_AMyTestProjectGameModeBase();
+	ENGINE_API UClass* Z_Construct_UClass_AGameModeBase();
+	UPackage* Z_Construct_UPackage__Script_MyTestProject();
+// End Cross Module References
+	void AMyTestProjectGameModeBase::StaticRegisterNativesAMyTestProjectGameModeBase()
+	{
+	}
+	UClass* Z_Construct_UClass_AMyTestProjectGameModeBase_NoRegister()
+	{
+		return AMyTestProjectGameModeBase::StaticClass();
+	}
+	struct Z_Construct_UClass_AMyTestProjectGameModeBase_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UE4CodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_AMyTestProjectGameModeBase_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_AGameModeBase,
+		(UObject* (*)())Z_Construct_UPackage__Script_MyTestProject,
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_AMyTestProjectGameModeBase_Statics::Class_MetaDataParams[] = {
+		{ "Comment", "/**\n * \n */" },
+		{ "HideCategories", "Info Rendering MovementReplication Replication Actor Input Movement Collision Rendering Utilities|Transformation" },
+		{ "IncludePath", "MyTestProjectGameModeBase.h" },
+		{ "ModuleRelativePath", "MyTestProjectGameModeBase.h" },
+		{ "ShowCategories", "Input|MouseInput Input|TouchInput" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_AMyTestProjectGameModeBase_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<AMyTestProjectGameModeBase>::IsAbstract,
+	};
+	const UE4CodeGen_Private::FClassParams Z_Construct_UClass_AMyTestProjectGameModeBase_Statics::ClassParams = {
+		&AMyTestProjectGameModeBase::StaticClass,
+		"Game",
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		UE_ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x009002ACu,
+		METADATA_PARAMS(Z_Construct_UClass_AMyTestProjectGameModeBase_Statics::Class_MetaDataParams, UE_ARRAY_COUNT(Z_Construct_UClass_AMyTestProjectGameModeBase_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_AMyTestProjectGameModeBase()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			UE4CodeGen_Private::ConstructUClass(OuterClass, Z_Construct_UClass_AMyTestProjectGameModeBase_Statics::ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(AMyTestProjectGameModeBase, 1401825242);
+	template<> MYTESTPROJECT_API UClass* StaticClass<AMyTestProjectGameModeBase>()
+	{
+		return AMyTestProjectGameModeBase::StaticClass();
+	}
+	static FCompiledInDefer Z_CompiledInDefer_UClass_AMyTestProjectGameModeBase(Z_Construct_UClass_AMyTestProjectGameModeBase, &AMyTestProjectGameModeBase::StaticClass, TEXT("/Script/MyTestProject"), TEXT("AMyTestProjectGameModeBase"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AMyTestProjectGameModeBase);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ MyTestProject/Intermediate/Build/Mac/x86_64/UE4Editor/Inc/MyTestProject/MyTestProjectGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef MYTESTPROJECT_MyTestProjectGameModeBase_generated_h
+#error "MyTestProjectGameModeBase.generated.h already included, missing '#pragma once' in MyTestProjectGameModeBase.h"
+#endif
+#define MYTESTPROJECT_MyTestProjectGameModeBase_generated_h
+
+#define MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_SPARSE_DATA
+#define MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_RPC_WRAPPERS
+#define MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMyTestProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_AMyTestProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AMyTestProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/MyTestProject"), NO_API) \
+	DECLARE_SERIALIZER(AMyTestProjectGameModeBase)
+
+
+#define MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAMyTestProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_AMyTestProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AMyTestProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/MyTestProject"), NO_API) \
+	DECLARE_SERIALIZER(AMyTestProjectGameModeBase)
+
+
+#define MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMyTestProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMyTestProjectGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMyTestProjectGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMyTestProjectGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMyTestProjectGameModeBase(AMyTestProjectGameModeBase&&); \
+	NO_API AMyTestProjectGameModeBase(const AMyTestProjectGameModeBase&); \
+public:
+
+
+#define MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMyTestProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMyTestProjectGameModeBase(AMyTestProjectGameModeBase&&); \
+	NO_API AMyTestProjectGameModeBase(const AMyTestProjectGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMyTestProjectGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMyTestProjectGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMyTestProjectGameModeBase)
+
+
+#define MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_12_PROLOG
+#define MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_SPARSE_DATA \
+	MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_RPC_WRAPPERS \
+	MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_INCLASS \
+	MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_SPARSE_DATA \
+	MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> MYTESTPROJECT_API UClass* StaticClass<class AMyTestProjectGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID MyTestProject_Source_MyTestProject_MyTestProjectGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
